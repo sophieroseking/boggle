9f0c5e518537b8c3e5b979ca44cbbf793645af41
Sophie King <sophiek89@hotmail.com>
2019-05-07 16:37:54 +0000

    added initial testing

--- test_boggle.py
+++ test_boggle.py
@@ -1,8 +1,35 @@
 import unittest
+import boggle
 
-
-class test_boggle(unittest.TestCase):
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 0)
+class TestBoggle(unittest.TestCase):
+    """
+    Our test suite for boggle solcer
+    """
+    
+    def test_can_create_an_empty_grid(self):
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boggle.make_grid(0,0)
+        self.assertEqual(len(grid),0)
+        
+    def test_grid_size_is_width_times_height(self):
+        """
+        Test is to ensure that the total size of the grid if equal to width times height
+        """
+        grid = boggle.make_grid(2,3)
+        self.assertEqual(len(grid), 6)
+    
+    def test_grid_coordinates(self):
+        """
+        Test to emnsure that all the coordinates inside of the grid can be accessed
+        """
+        grid = boggle.make_grid(2,2)
+        self.assertIn((0,0), grid)
+        self.assertIn((0,1), grid)
+        self.assertIn((1,0), grid)
+        self.assertIn((1,1), grid)
+        self.assertNotIn((2,2), grid)
+        
         
         
